a442466b68159bd80acd4580b3984c1d9bac2de7
Ritianne <ritianne.cardona.e22960@mcast.edu.mt>
2021-01-03 02:59:42 +0100

    Home Assignment Task 1 Part 1
    Import Assets, Setup Camera, Create border, Move Player, Scrolling Background

---
+++ Home/CG/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 0.15f;
+    float xMin, xMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        this.transform.position = new Vector2(newXPos, transform.position.y);
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        //yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        //yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+    }
+}
