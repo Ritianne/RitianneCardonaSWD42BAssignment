3d1dfbc0c017630a438b1d340cfa8be111d8d545
Ritianne <ritianne.cardona.e22960@mcast.edu.mt>
2021-01-17 01:16:20 +0100

    Home Assignment Part 2 Part 1
    Explosion Particles on Enemy and Player death, Destroy Enemy on collision and play sound, Decrease player health on collision with enemy

--- Home/CG/Assets/Scripts/Player.cs
+++ Home/CG/Assets/Scripts/Player.cs
@@ -6,7 +6,15 @@ public class Player : MonoBehaviour
 {     
     [SerializeField] float moveSpeed = 2f;
     [SerializeField] float padding = 0.7f;
-    [SerializeField] float health = 5f;
+    [SerializeField] float health = 50f;
+    [SerializeField] int points = 0;
+
+    [SerializeField] AudioClip collisionSound;
+    [SerializeField] AudioClip noCollisionSound;
+    [SerializeField] [Range(0, 1)] float SoundVolume = 0.75f;
+
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float explosionDuration = 1f;
 
     float xMin, xMax;
 
@@ -49,16 +57,28 @@ public class Player : MonoBehaviour
         }
 
         ProcessHit(dmgDealer);
+        Destroy(collision.gameObject);
+        GameObject explosion = Instantiate(deathVFX, collision.gameObject.transform.position, Quaternion.identity);
     }
 
     // Send DamageDealer details
     private void ProcessHit(DamageDealer dmgDealer)
     {
         health -= dmgDealer.GetDamage();
+        AudioSource.PlayClipAtPoint(collisionSound, Camera.main.transform.position, SoundVolume);
 
         if (health <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        // Instiantiate Explosion Effects
+        GameObject explosion = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        // Destroy after 1 second
+        Destroy(explosion, explosionDuration);
     }
 }
